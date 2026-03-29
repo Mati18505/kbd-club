3a1b323d7d146f8983bcb8ec25bbf4da20d4412f
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-03-29 22:33:07 +0200

    first version

---
+++ html/image-magnifier.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  $(".zoomable-img").on("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let src = $(this).children("img").attr("src");
+    console.log(src);
+
+    $(".popup-img").css({"display": "block"});
+    $(".popup-img img").attr("src", src);
+  });
+
+  $(".popup-img span").on("click", function(_e) {
+    $(".popup-img").css({"display": "none"});
+  });
+
+  $(".popup-img img").on("click", function(e) {
+    e.stopPropagation();
+  });
+
+  $(window).on("click", function() { 
+    if ($(".popup-img").css("display") != "none") {
+      $(".popup-img").css({"display": "none"});
+    }
+  });
+
+  $(document).on('keyup', function(e) {
+    if (e.key == "Escape") {
+      $(".popup-img").css({"display": "none"});
+    }
+  });
+});
